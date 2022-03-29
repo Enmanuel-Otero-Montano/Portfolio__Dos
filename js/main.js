b9737ddc4f2318fd588e4c26a1d278eabea7b8a6
Enmanuel Otero M <oteromontanoenmanuel@gmail.com>
2022-03-29 13:27:34 -0300

    imagenes en switch, agregados sections

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const inputMoon = document.querySelector(".moon")
 const switchMode = document.querySelector(".switch")
 const body = document.querySelector("body")
 const overlay = document.querySelector(".body-overlay")
+const heroName = document.querySelector(".hero__name-hidden")
+const navLinks = document.querySelectorAll(".nav-links")
 
 menu.addEventListener("click", ()=> {
     hamburger[0].classList.toggle("top")
@@ -45,7 +47,28 @@ inputSun.addEventListener("click", ()=> {
     switchMode.classList.remove("switch-dark")
 })
 
+for(const navLink of navLinks) {
+    navLink.addEventListener("click", ()=> {
+        hamburger[0].classList.remove("top")
+        hamburger[1].classList.remove("middle")
+        hamburger[2].classList.remove("botton")
+        nav.classList.add("nav-hidden")
+        overlay.classList.remove("body-overlay-show")
+    })
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    setTimeout(()=> {
+        showHeroName()
+    }, 1100)
+})
+
 //Funcion que interviene en el proceso de cambio de tamaño de la letra musical
 const re = ()=> {
     musicalLyrics.classList.remove("musical-lyrics-active")
+}
+
+const showHeroName = ()=> {
+    heroName.classList.remove("hero__name-hidden")
+    heroName.classList.add("hero__name")
 }
